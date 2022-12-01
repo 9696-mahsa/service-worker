d2d18fe341bf82d07a6bd331e4576dac26bf502b
Mahsa <mahsa.behmanesh74@gmail.com>
2022-12-01 19:14:28 +0330

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi...
+          Hi... I'm a PWA ;)
         </p>
       </header>
     </div>
